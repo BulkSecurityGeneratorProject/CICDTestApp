ea680599b11894738742d93286f488ae03e9e9ce
Ian Muturi [ICT] <imuturi@CO-OPBANK.CO.KE>
2018-10-26 12:48:19 +0300

    sorted html bug

---
+++ src/main/java/com/coopbank/limits/web/rest/AngularJsForwardController.java
@@ -0,0 +1,14 @@
+package com.coopbank.limits.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AngularJsForwardController {
+	
+ @RequestMapping(value = "/**/{[path:[^\\.]*}")
+	    public String redirect() {
+	        return "forward:/";
+	    }
+
+}
